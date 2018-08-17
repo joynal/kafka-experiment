2f355d4db3f97c767d36840e203d64a074b4c519
Joynal Abedin <joynal3483@gmail.com>
2018-08-17 16:13:13 +0600

    eslint fix

--- app/consumers/klaviyo.js
+++ app/consumers/klaviyo.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const fetch = require('node-fetch');
 const queryString = require('querystring');
 
-const producer = require('../producers/root');
+const producer = require('../producer');
 const { sendToQueue } = require('../utils');
 
 module.exports = async (message, nextTopic = null) => {
@@ -10,28 +11,29 @@ module.exports = async (message, nextTopic = null) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        "Accept": "application/json"
+        Accept: 'application/json',
       },
       body: queryString.stringify({
         api_key: message.api_key,
         email: message.email,
         properties: JSON.stringify(message.properties),
-        confirm_optin: message.confirm_optin
+        confirm_optin: message.confirm_optin,
       }),
     });
 
-    let json = await response.json();
-    console.log("statusCode >>", response.status);
-    console.log("Klaviyo response >>", json);
+    const json = await response.json();
+    console.log('statusCode >>', response.status);
+    console.log('Klaviyo response >>', json);
     if (response.status !== 200) throw new Error(json.message);
   } catch (err) {
     console.log(err.message);
-    
+
     if (nextTopic) {
       console.log(`Sending message to ${nextTopic}`);
-      message.timestamp = Date.now();
-      message.failingReason = err.message;
-      sendToQueue(producer, nextTopic, JSON.stringify(message));
+      const messageCopy = message;
+      messageCopy.timestamp = Date.now();
+      messageCopy.failingReason = err.message;
+      sendToQueue(producer, nextTopic, JSON.stringify(messageCopy));
     }
   }
 };
