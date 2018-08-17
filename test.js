495fae22bb6488d9518acf156b26005b1672a54c
Joynal Abedin <joynal3483@gmail.com>
2018-08-17 16:13:33 +0600

    moved test data file

--- test.js
+++ test.js
@@ -1,22 +1,19 @@
-'use strict';
-
 require('dotenv').config();
-
 const faker = require('faker');
-const producer = require('./producers/root');
-const { sendToQueue } = require('./utils');
 
-for (let i=0; i <= 100; i++){
+const producer = require('./app/producer');
+const { sendToQueue } = require('./app/utils');
+
+for (let i = 0; i <= 100; i += 1) {
   sendToQueue(producer, 'providers', [
     JSON.stringify({
       api_key: process.env.KLAVIYO_API_KEY,
       email: faker.internet.email(),
       properties: {
         $first_name: faker.name.firstName(),
-        $last_name: faker.name.lastName()
+        $last_name: faker.name.lastName(),
       },
-      confirm_optin: false
-    })
+      confirm_optin: false,
+    }),
   ]);
-
 }
