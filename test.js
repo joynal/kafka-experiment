5d38ec74461c37ad88496419df18f495888ba9d6
Joynal Abedin <joynal3483@gmail.com>
2019-02-11 23:13:45 +0600

    incereased producing number

--- test.js
+++ test.js
@@ -5,16 +5,14 @@ const config = require('./src/config');
 const producer = require('./src/helpers/producer');
 const { sendToQueue } = require('./src/helpers/utils');
 
-for (let i = 0; i <= 10; i += 1) {
-  sendToQueue(producer, 'provider', [
-    JSON.stringify({
-      api_key: config.klaviyoApiKey,
-      email: faker.internet.email(),
-      properties: {
-        $first_name: faker.name.firstName(),
-        $last_name: faker.name.lastName(),
-      },
-      confirm_optin: false,
-    }),
-  ]);
+for (let i = 0; i < 20000; i += 1) {
+  sendToQueue(producer, 'provider', JSON.stringify({
+    api_key: config.klaviyoApiKey,
+    email: faker.internet.email(),
+    properties: {
+      $first_name: faker.name.firstName(),
+      $last_name: faker.name.lastName(),
+    },
+    confirm_optin: false,
+  }));
 }
